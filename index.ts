82a50aa9d958c2cfb53cbac817ca042ba2c168f9
Juan Bautista Vega <jv604824@gmail.com>
2024-08-30 12:40:31 -0300

    test-1

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { firestore, rtdb } from "./db";
-import * as express from "express";
-import * as cors from "cors";
+import express from "express";
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
 
